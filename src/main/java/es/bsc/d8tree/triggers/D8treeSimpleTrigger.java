a89ce76086fec8f1ea54d27d6e5e39bee8cea8dd
cugni <cesare.cugnasco@gmail.com>
2016-04-21 16:33:57 +0200

    modified the trigger in order to handle correctly also special columns such as Maps etc..

--- src/main/java/es/bsc/d8tree/triggers/D8treeSimpleTrigger.java
+++ src/main/java/es/bsc/d8tree/triggers/D8treeSimpleTrigger.java
@@ -1,19 +1,13 @@
 package es.bsc.d8tree.triggers;
 
-import org.apache.cassandra.db.Clustering;
+import org.apache.cassandra.config.CFMetaData;
+import org.apache.cassandra.db.ClusteringComparator;
+import org.apache.cassandra.db.DecoratedKey;
 import org.apache.cassandra.db.Mutation;
-import org.apache.cassandra.db.RowUpdateBuilder;
-import org.apache.cassandra.db.filter.ColumnFilter;
 import org.apache.cassandra.db.partitions.Partition;
-import org.apache.cassandra.db.rows.Cell;
-import org.apache.cassandra.db.rows.Row;
-import org.apache.cassandra.db.rows.Unfiltered;
-import org.apache.cassandra.db.rows.UnfilteredRowIterator;
+import org.apache.cassandra.db.partitions.PartitionUpdate;
 import org.apache.cassandra.triggers.ITrigger;
 import org.apache.cassandra.utils.ByteBufferUtil;
-import org.apache.cassandra.utils.FBUtilities;
-import org.apache.cassandra.utils.SearchIterator;
-import org.apache.cassandra.utils.btree.BTreeSearchIterator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,54 +20,41 @@ import java.util.Collections;
  * Simple - stateless - trigger for the D8tree.
  */
 public class D8treeSimpleTrigger implements ITrigger {
-    private static final int TREE_HEIGHT = Integer.getInteger("loop_tree.height",10);
+    private static final int TREE_HEIGHT = Integer.getInteger("loop_tree.height", 10);
 
+    private final static Logger log = LoggerFactory.getLogger(D8treeSimpleTrigger.class);
 
-    private final static Logger log= LoggerFactory.getLogger(D8treeSimpleTrigger.class);
     public Collection<Mutation> augment(Partition partition) {
-        final  String cf=partition.metadata().cfName;
-        if(partition.isEmpty()){
-            return  null;
+        log.debug("Partition is of {} type", partition.getClass().getSimpleName());
+        final String cf = partition.metadata().cfName;
+        if (partition.isEmpty()) {
+            return null;
         }
-        if(!cf.matches(".*_d8tree$")){
-            log.warn("D8treeSimpleTrigger called on the wrong table: {}",cf);
+        if (!cf.matches(".*_d8tree$")) {
+            log.warn("D8treeSimpleTrigger called on the wrong table: {}", cf);
             return null;
         }
         try {
             String key = ByteBufferUtil.string(partition.partitionKey().getKey());
-            if(key.length()<TREE_HEIGHT){
+            if (key.length() < TREE_HEIGHT) {
                 log.debug("Not leaf insertions, let's ignore it.");
                 return null;
-
-            }else{
-
-                ArrayList<Mutation> mutations = new ArrayList<Mutation>();
-                for(int i=1; i<key.length(); i++){
-                    RowUpdateBuilder audit =
-                            new RowUpdateBuilder(partition.metadata(),
-                            FBUtilities.timestampMicros(),
-                            key.substring(0,i));
-
-
-                    UnfilteredRowIterator unfilteredRowIterator = partition.unfilteredIterator();
-
-
-                    while (unfilteredRowIterator.hasNext()) {
-                        Unfiltered unfiltered = unfilteredRowIterator.next();
-                        if(unfiltered.isRow()) {
-                            Row row=(Row)unfiltered;
-                            audit.clustering((Object[]) row.clustering().getRawValues());
-                            for (Cell cell :row.cells()){
-                                audit.add(cell.column(), cell.value());
-                            }
-                        }
+            } else {
+                ArrayList<Mutation> mutations = new ArrayList<>();
+                if (partition instanceof PartitionUpdate) {
+                    PartitionUpdate pu = (PartitionUpdate) partition;
+                    ClusteringComparator comparator = pu.metadata().getKeyValidatorAsClusteringComparator();
+                    for (int i = 1; i < key.length(); i++) {
+                        DecoratedKey dk = pu.metadata().partitioner.decorateKey(CFMetaData.serializePartitionKey(comparator.make(key.substring(0, i))));
+                        PartitionUpdate rows = new PartitionUpdate(pu.metadata(), dk, pu.columns(), pu.operationCount());
+                        pu.iterator().forEachRemaining(rows::add);
+                        mutations.add(new Mutation(rows));
                     }
-
-                    mutations.add(audit.build());
-
+                } else {
+                    log.warn("Partition it is not an PartitionUpdate");
                 }
+                log.debug("Returning {} mutations", mutations.size());
                 return Collections.unmodifiableList(mutations);
-
             }
 
         } catch (CharacterCodingException e) {
